95858436a2b1cf5d76bbffdb2705333774255b4e
fpapadopou <fot2803@hotmail.com>
2014-08-29 20:25:10 +0300

    Grant permissions for creating symlink to /var/www in install script

--- scripts/apache_install.sh
+++ scripts/apache_install.sh
@@ -4,7 +4,7 @@ set -e
 
 PACKAGENAME=builder
 
-ln -s /opt/codebender/$PACKAGENAME /var/www/$PACKAGENAME
+sudo ln -s /opt/codebender/$PACKAGENAME /var/www/$PACKAGENAME
 sudo cp /opt/codebender/$PACKAGENAME/apache-config /etc/apache2/sites-available/codebender
 cd /etc/apache2/sites-enabled
 sudo ln -s ../sites-available/codebender 00-codebender
