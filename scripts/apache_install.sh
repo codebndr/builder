60e9905632299dfb8b9edfc0bb8133c4791033ea
fpapadopou <fot2803@hotmail.com>
2014-08-29 21:19:02 +0300

    Added restart after apache config.

--- scripts/apache_install.sh
+++ scripts/apache_install.sh
@@ -8,3 +8,4 @@ sudo ln -s /opt/codebender/$PACKAGENAME /var/www/$PACKAGENAME
 sudo cp /opt/codebender/$PACKAGENAME/apache-config /etc/apache2/sites-available/$PACKAGENAME
 cd /etc/apache2/sites-enabled
 sudo ln -s ../sites-available/$PACKAGENAME 00-codebender
+sudo service apache2 restart
