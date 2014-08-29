cd61dd54b6b54a4110603d91d6c0e351b68b61ba
fpapadopou <fot2803@hotmail.com>
2014-08-29 21:31:01 +0300

    Fixed apache config.

--- scripts/apache_install.sh
+++ scripts/apache_install.sh
@@ -4,8 +4,8 @@ set -e
 
 PACKAGENAME=builder
 
-sudo ln -s /opt/codebender/$PACKAGENAME /var/www/$PACKAGENAME
-sudo cp /opt/codebender/$PACKAGENAME/apache-config /etc/apache2/sites-available/$PACKAGENAME
+sudo ln -s /opt/codebender/$PACKAGENAME/Symfony/web /var/www/$PACKAGENAME
+sudo cp /opt/codebender/$PACKAGENAME/apache-config /etc/apache2/sites-available/codebender-$PACKAGENAME
 cd /etc/apache2/sites-enabled
-sudo ln -s ../sites-available/$PACKAGENAME 00-codebender
+sudo ln -s ../sites-available/codebender-$PACKAGENAME 00-codebender
 sudo service apache2 restart
