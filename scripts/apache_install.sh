1330782f4c568e361e034481256472cb66199dfe
Fotis Papadopoulos <fot2803@hotmail.com>
2015-10-14 18:41:09 +0300

    Apache 2.4.7 (default on Unbuntu 14.04) requires VirtualHost files to have a '.conf' extension

--- scripts/apache_install.sh
+++ scripts/apache_install.sh
@@ -7,5 +7,5 @@ PACKAGENAME=builder
 sudo ln -s /opt/codebender/$PACKAGENAME/Symfony/web /var/www/$PACKAGENAME
 sudo cp /opt/codebender/$PACKAGENAME/apache-config /etc/apache2/sites-available/codebender-$PACKAGENAME
 cd /etc/apache2/sites-enabled
-sudo ln -s ../sites-available/codebender-$PACKAGENAME 00-codebender
+sudo ln -s ../sites-available/codebender-$PACKAGENAME 00-codebender.conf
 sudo service apache2 restart
