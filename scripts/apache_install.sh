1062d777a61b66db589845a8fb1a2c46aeb6fe56
Fotis Papadopoulos <fpapadopou@gmail.com>
2015-10-22 13:54:18 +0300

    Removed unused symlink

--- scripts/apache_install.sh
+++ scripts/apache_install.sh
@@ -4,7 +4,6 @@ set -e
 
 PACKAGENAME=builder
 
-sudo ln -s /opt/codebender/$PACKAGENAME/Symfony/web /var/www/$PACKAGENAME
 sudo cp /opt/codebender/$PACKAGENAME/apache-config /etc/apache2/sites-available/codebender-$PACKAGENAME
 cd /etc/apache2/sites-enabled
 sudo ln -s ../sites-available/codebender-$PACKAGENAME 00-codebender.conf
