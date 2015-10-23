fdf6d21d8487edda3061a55213effcd8e9afe9dc
Fotis Papadopoulos <fot2803@hotmail.com>
2015-10-23 15:59:40 +0300

    Removed OS X related if-else's

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,26 +4,25 @@ set -e
 
 PACKAGENAME=builder
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	echo "Configuring environment for Linux"
-	# Make sure we have up-to-date stuff
-    sudo apt-get update
-	if [[ ! $TRAVIS ]]; then
-		# Ubuntu Server (on AWS?) lacks UTF-8 for some reason. Give it that
-		sudo locale-gen en_US.UTF-8
-		sudo apt-get install -y php5-intl
-	fi
-	# Install dependencies
-	sudo apt-get install -y apache2 libapache2-mod-php5 php-pear php5-curl php5-sqlite php5-mysql acl curl git
-	# Enable Apache configs
-	sudo a2enmod rewrite
-    sudo a2enmod alias
-    # Restart Apache
-    sudo service apache2 restart
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-	# is there something comparable to this on os x? perhaps Homebrew
-	echo "Configuring environment for OS X (to be added..)"
+if [[ "$OSTYPE" != "linux-gnu" ]]; then
+    echo "Only Linux environment is supported"
 fi
+echo "Configuring environment for Linux"
+# Make sure we have up-to-date stuff
+sudo apt-get update
+if [[ ! $TRAVIS ]]; then
+    # Ubuntu Server (on AWS?) lacks UTF-8 for some reason. Give it that
+    sudo locale-gen en_US.UTF-8
+    sudo apt-get install -y php5-intl
+fi
+# Install dependencies
+sudo apt-get install -y apache2 libapache2-mod-php5 php-pear php5-curl php5-sqlite php5-mysql acl curl git
+# Enable Apache configs
+sudo a2enmod rewrite
+sudo a2enmod alias
+# Restart Apache
+sudo service apache2 restart
+
 
 if [[ ! $TRAVIS ]]; then
 
@@ -47,23 +46,14 @@ cd /opt/codebender/$PACKAGENAME
 rm -rf Symfony/app/cache/*
 rm -rf Symfony/app/logs/*
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
+if [[ ! $TRAVIS ]]; then
+    # Need to create cache and logs directories, as they do not pre-exist in new deployments
+    mkdir -p `pwd`/Symfony/app/cache/
+    mkdir -p `pwd`/Symfony/app/logs/
 
-	if [[ ! $TRAVIS ]]; then
-	    # Need to create cache and logs directories, as they do not pre-exist in new deployments
-	    mkdir -p `pwd`/Symfony/app/cache/
-		mkdir -p `pwd`/Symfony/app/logs/
-
-        # Set access control for both apache and current user on cache and logs directories
-		sudo setfacl -R -m u:www-data:rwX -m u:`whoami`:rwX `pwd`/Symfony/app/cache `pwd`/Symfony/app/logs
-		sudo setfacl -dR -m u:www-data:rwx -m u:`whoami`:rwx `pwd`/Symfony/app/cache `pwd`/Symfony/app/logs
-	fi
-
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-
-	HTTPDUSER=`ps aux | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
-	sudo chmod +a "$HTTPDUSER allow delete,write,append,file_inherit,directory_inherit" Symfony/app/cache Symfony/app/logs
-	sudo chmod +a "`whoami` allow delete,write,append,file_inherit,directory_inherit" Symfony/app/cache Symfony/app/logs
+    # Set access control for both apache and current user on cache and logs directories
+    sudo setfacl -R -m u:www-data:rwX -m u:`whoami`:rwX `pwd`/Symfony/app/cache `pwd`/Symfony/app/logs
+    sudo setfacl -dR -m u:www-data:rwx -m u:`whoami`:rwx `pwd`/Symfony/app/cache `pwd`/Symfony/app/logs
 fi
 
 cd Symfony
