02246b321557d56754a5756dd36dc3414bc55c5b
Fotis Papadopoulos <fot2803@hotmail.com>
2015-10-23 16:08:32 +0300

    Renamed apache configuration file

--- scripts/apache_install.sh
+++ scripts/apache_install.sh
@@ -5,7 +5,7 @@ set -e
 PACKAGENAME=builder
 
 sudo ln -s /opt/codebender/$PACKAGENAME/Symfony/web /var/www/$PACKAGENAME
-sudo cp /opt/codebender/$PACKAGENAME/apache-config /etc/apache2/sites-available/codebender-$PACKAGENAME
+sudo cp /opt/codebender/$PACKAGENAME/apache-config-2.4 /etc/apache2/sites-available/codebender-$PACKAGENAME
 cd /etc/apache2/sites-enabled
 sudo ln -s ../sites-available/codebender-$PACKAGENAME 00-codebender.conf
 sudo service apache2 restart
